22b2e83690898e00f84b2e04ab5454187c446aad
Alexsander <alexdonay@gmail.com>
2023-06-15 20:20:27 -0300

    Classe de usuario

--- app/src/main/java/igor/moreira/codeca/UserModel.java
+++ app/src/main/java/igor/moreira/codeca/UserModel.java
@@ -6,4 +6,52 @@ public class UserModel {
     private String senha;
     private String CPF;
     private String telefone;
+
+    public UserModel(String nome, String eMail, String senha, String CPF, String telefone) {
+        this.nome = nome;
+        this.eMail = eMail;
+        this.senha = senha;
+        this.CPF = CPF;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }

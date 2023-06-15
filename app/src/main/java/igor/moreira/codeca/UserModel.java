c1e135dae1361618fb3b6ac36b47a3105ba40f8b
Alexsander <alexdonay@gmail.com>
2023-06-15 20:17:38 -0300

    Renomeando layout

---
+++ app/src/main/java/igor/moreira/codeca/UserModel.java
@@ -0,0 +1,9 @@
+package igor.moreira.codeca;
+
+public class UserModel {
+    private String nome;
+    private String eMail;
+    private String senha;
+    private String CPF;
+    private String telefone;
+}

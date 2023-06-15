ff36e3b2028fdec1e46e428ad34d2b5bd527916e
Alexsander <alexdonay@gmail.com>
2023-06-15 20:12:33 -0300

    Renomeando layout

--- app/src/main/java/igor/moreira/codeca/LoginActivity.java
+++ app/src/main/java/igor/moreira/codeca/LoginActivity.java
@@ -1,28 +1,11 @@
 package igor.moreira.codeca;
-
 import android.os.Bundle;
-import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-
-import igor.moreira.codeca.databinding.LoginBinding;
-
 public class LoginActivity extends AppCompatActivity {
-
-    LoginBinding binding;
-    GoogleSignInClient googleSignInClient;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = LoginBinding.inflate(getLayoutInflater());
-
-        View view = binding.getRoot();
-
-        setContentView(view);
+        setContentView(R.layout.activity_solicitacao);
     }
 }
 
--- app/src/main/java/igor/moreira/codeca/SplashActivity.java
+++ app/src/main/java/igor/moreira/codeca/SplashActivity.java
@@ -2,13 +2,23 @@ package igor.moreira.codeca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
-
+    private static final long SPLASH_TIMEOUT = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
     }
 }

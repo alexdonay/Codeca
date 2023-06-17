d2b68f685d7f95085989a3db62b5e6aa911e2e68
Alexsander <alexdonay@gmail.com>
2023-06-16 22:50:53 -0300

    cadastro de usuário, banco de dados

--- app/src/main/java/igor/moreira/codeca/CadastroActivity.java
+++ app/src/main/java/igor/moreira/codeca/CadastroActivity.java
@@ -3,6 +3,7 @@ package igor.moreira.codeca;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,8 +21,11 @@ public class CadastroActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_cadastro);
+
         btnCadastrar = (Button) findViewById(R.id.btnCadastrar2);
+        Log.d("MinhaActivity", "Entrando na Activity cadastro");
         btnCadastrar.setOnClickListener(view -> {
             BdSingleton bd = BdSingleton.getInstance();
             txNome = findViewById(R.id.txtNome);
@@ -36,7 +40,10 @@ public class CadastroActivity extends AppCompatActivity {
             } else {
                 ModeloUsuario usuario = new ModeloUsuario(txNome.getText().toString(), txEmail.getText().toString(),
                         txSenha.getText().toString(), txCPF.getText().toString(), txCelular.getText().toString());
-                bd.addUsuario(usuario);
+                bd.addUsuario(usuario);//Envia para a api
+                UsuarioDbHelper userDB = new UsuarioDbHelper(this,null,null,1);
+                userDB.criarUsuario(usuario);
+                userDB.close();
                 Intent intent = new Intent(CadastroActivity.this, SolicitacaoActivity.class);
                 startActivity(intent);
                 finish();
--- app/src/main/java/igor/moreira/codeca/SplashActivity.java
+++ app/src/main/java/igor/moreira/codeca/SplashActivity.java
@@ -5,13 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
-    private static final long SPLASH_TIMEOUT = 2000;
+    private static final long SPLASH_TIMEOUT = 12222;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Log.d("MinhaActivity", "Entrando na Activity splash");
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
